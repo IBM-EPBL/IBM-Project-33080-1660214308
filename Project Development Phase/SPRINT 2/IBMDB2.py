aa9e6e871c55fa1430bef4d10ec1fe6bc7889759
sanjaikumarsk <113848206+sanjaikumarsk@users.noreply.github.com>
2022-11-16 16:12:23 +0530

    Add files via upload

---
+++ Project Development Phase/SPRINT 2/IBMDB2.py
@@ -0,0 +1,30 @@
+import ibm_db
+dsn_hostname = "19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+dsn_uid = "lcp09836"
+dsn_pwd = "fd6Bk3t9bwD6O0iu"
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "BLUDB"
+dsn_port = "30699"
+dsn_protocol = "TCPIP"
+dsn_security = "SSL"
+
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security)
+
+
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+
+except:
+    print ("Unable to connect: ", ibm_db.conn_errormsg() )
+
+
